8ccdb61f51ec318024aeaab161f5a915f04eee66
Binh <binhwork245@gmail.com>
2025-05-28 07:40:17 +0700

    fix: multi call to EVM

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     if(!isLoading)
       setDecreasedOERFromEthereum();
-  }, [isLoading, setDecreasedOERFromEthereum]);
+  }, [isLoading]);
   
   useEffect(() => {
   }, [OERDecreasedOnVote]);
